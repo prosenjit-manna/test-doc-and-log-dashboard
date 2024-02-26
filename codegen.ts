06278af8a6e7b5e96d0ed4751f54f01df502ecad
Prosenjit Manna <prosenjit@itobuz.com>
2024-02-26 12:44:22 +0530

    env updated

--- codegen.ts
+++ codegen.ts
@@ -1,7 +1,7 @@
 import { CodegenConfig } from '@graphql-codegen/cli'
  
 const config: CodegenConfig = {
-  schema: 'http://192.168.68.152:1337/graphql',
+  schema: 'http://192.168.66.1:1337/graphql',
   documents: ['src/**/*.tsx'],
   ignoreNoDocuments: true, // for better experience with the watcher
   generates: {
